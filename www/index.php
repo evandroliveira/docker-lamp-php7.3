935b91690770d3ba0cc1cdf515b778e1991eba77
Erik Tonon <eriktonon@gmail.com>
2019-03-29 11:46:51 -0300

    Update

--- www/index.php
+++ www/index.php
@@ -2,3 +2,8 @@
 
 echo '<h1>Olá Amigos!!!</h1>';
 echo '<p> Obrigado por baixar meu projeto!!!</p>';
+echo "<h3>Abaixo estão os drivers PDO que estão habilitados</h3>";
+echo "<a href=http://localhost/info.php/>";
+echo "<pre>";
+print_r(PDO::getAvailableDrivers());
+
